5147c9b62cb91d02a5a63cecc2fe4feaa0e75634
Matheus Benedito Mendes <matheusbenedito.mendes@gmail.com>
2025-08-20 21:55:59 -0300

    icone de menu

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common'
 import { CarApiService } from './services/car-api.service';
+import { NavbarComponent } from './core/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet ],
+  imports: [ RouterOutlet, NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- front/src/app/core/home.component.ts
+++ front/src/app/core/home.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styles: ``
 })
-export class HomeComponent {
-
-}
+export class HomeComponent { }
